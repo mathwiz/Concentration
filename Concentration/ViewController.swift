72644a1e6c2edb985b39175a22d7dcbee6ac027b
Yohan Lee <yohanlee@yahoo.com>
2019-10-09 23:09:39 -0400

    Cards added

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    var emojiChoices = ["♔", "♕", "♖", "♘", "♗", "♙", "♔", "♕", "♖", "♘", "♗", "♙"]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            print("cardNumber is \(String(describing: cardNumber))")
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }
     }
 
-
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        print("flipCard(withEmoji: \(emoji))")
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = UIColor.orange
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = UIColor.white
+        }
+    }
 }
 
